d2be84d2c0c8302e813621a8475953cd860d2237
fgouvea11 <51537630+fgouvea11@users.noreply.github.com>
2019-12-15 18:10:37 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*function escrevendo(){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textArray.forEach((letra, i) =>{
+        setTimeout(() => elemento.innerHTML += letra, 75*i);
+    });
+}
+
+const titulo = document.getElementById('escrever');
+escrevendo(titulo);*/
+
+function database(){
+    alert('Esta compra requer negociação.')
+}
